92d6de6bee3ed62367bb9d1ed76d284ad7a8c3f0
wjspmy <1426208873@qq.com>
2018-09-27 18:22:05 +0800

    update

---
+++ test/4/print_path.h
@@ -0,0 +1,19 @@
+/*************************************************************************
+    > File Name: print_path.h
+    > Author: 潘梦园
+    > brief:  
+    > Created Time: 2018年09月26日 星期三 23时08分57秒
+ ************************************************************************/
+
+#ifndef _LEARN_TEST_4_PRINT_PATH_H
+#define _LEARN_TEST_4_PRINT_PATH_H
+
+#include <stdint.h>
+
+struct Node {
+    int32_t value;
+    Node* right;
+    Node* left;:
+}
+
+
