e7822b2ef0cf46f0cf7a8bccddf8a58ae05e4d93
wjspmy <1426208873@qq.com>
2018-10-23 23:01:14 +0800

    update

---
+++ test/20/max_substring.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+    > File Name: max_substring.cpp
+    > Author: 潘梦园
+    > brief:  
+    > Created Time: 2018年10月19日 星期五 22时58分02秒
+ ************************************************************************/
+
+#include <iostream>
+#include "max_substring.h"
+
+int32_t max_substring(char* c1, char* c2) {
+
+}
+
---
+++ test/20/max_substring.h
@@ -0,0 +1,15 @@
+/*************************************************************************
+    > File Name: max_substring.h
+    > Author: 潘梦园
+    > brief:  
+    > Created Time: 2018年10月19日 星期五 22时58分02秒
+ ************************************************************************/
+
+#ifndef _LEARN_TEST_20_MAX_SUBSTRING_H_
+#define _LEASRN_TEST_20_MAX_SUBSTRING_H_
+
+#include <stdint.h>
+#include <string>
+
+
+#endif
