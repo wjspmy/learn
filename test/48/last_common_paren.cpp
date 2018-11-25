4551876c80197649d763620ebddfde4f4be4dbf1
wjspmy <1426208873@qq.com>
2018-11-25 23:19:29 +0800

    update

---
+++ test/48/last_common_paren.cpp
@@ -0,0 +1,13 @@
+/*************************************************************************
+    > File Name: last_common_paren.cpp
+    > Author: 潘梦园
+    > brief:  
+    > Created Time: 2018年11月25日 星期日 21时41分08秒
+ ************************************************************************/
+
+#include <iostream>
+#include "last_common_parent.h"
+
+Tree* creat(Tree* root, int32_t value) {
+    Tree* root;
+}
---
+++ test/48/last_common_parent.h
@@ -0,0 +1,21 @@
+/*************************************************************************
+    > File Name: last_common_parent.h
+    > Author: 潘梦园
+    > brief:  
+    > Created Time: 2018年11月25日 星期日 21时41分08秒
+ ************************************************************************/
+
+#ifndef _LEARN_TEST_47_LAST_COMMON_PARENT_H_
+#define _LEARN_TEST_47_LAST_COMMON_PARENT_H_
+
+#include <stdint.h>
+
+
+struct Tree {
+    int32_t data;
+    Tree* next;
+};
+
+Tree* creat();
+
+#endif
