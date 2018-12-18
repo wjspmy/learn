00c373e033e2c2ff029a63a6e11dad8453cb27fe
wjspmy <1426208873@qq.com>
2018-12-18 23:09:37 +0800

    增加插入

--- c++_个人记账/pmy_mysql.cpp
+++ c++_个人记账/pmy_mysql.cpp
@@ -7,16 +7,22 @@
 
 #include <iostream>
 #include <string.h>
+#include <string>
+#include <stdio.h>
+
 #include <mysql/mysql.h>
 
+const int MAX = 20;
 MYSQL mysql;
+
+
 
 bool login()
 {
 	mysql_init(&mysql);  //连接mysql，数据库  
 	const char host[] = "localhost";
 	char user[20];
-	char psw[20];
+    char psw[20];
 	const char table[] = "test";
 	const int port = 3306;
     while (strcmp(user, "root") != 0 || strcmp(psw, "123456.+") != 0) {
@@ -24,9 +30,12 @@ bool login()
         std::cout << "Please input username: ";
         std::cin >> user;
         std::cout << "Please input the password: ";
+        system("stty -echo");
         std::cin >> psw;
+        system("stty echo");
+        std::cout << std::endl;
         if (strcmp(user, "root") != 0 || strcmp(psw, "123456.+") != 0) {
-            std::cout << "error!\n";
+            std::cout << "用户名或密码错误!\n";
         }
         
     }
@@ -43,8 +52,43 @@ bool login()
 	}
 }
 
+//插入数据
+void write() {
+    //insert into student(id, name, age, address) values()
+    std::string data = "insert into student(id, name, age, address) values('";
+    std::string id;
+    std::string age;
+    std::string name;
+    std::string address;
+    std::cout << "请输入学号：";
+    std::cin >> id;
+    std::cout << "请输入姓名：";
+    std::cin >> name;
+    std::cout << "请输入年龄：";
+    std::cin >> age;
+    std::cout << "请输入地址：";
+    std::cin >> address;
+
+    
+    data += id + "', '";
+    data += name + "', '";
+    data += age + "', '";
+    data += address + "');";
+    std::cout << data << std::endl;
+
+    if (mysql_query(&mysql, data.c_str())) {
+        std::cout << "插入失败\n";
+    } else{
+        std::cout << "插入成功\n"; 
+    }
+
+}
+
+
+
 int main() {
 	bool sign = login();
+    write();
 	return 0;
 }
 	
